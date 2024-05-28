ec1ae5ed6cfc6788f5c71eb9f0d16d87a5cc3288
x-poison <abdulrahmanmossadhacker@gmail.com>
2024-05-28 12:09:41 -0400

    Done💌

--- start.js
+++ start.js
@@ -1,21 +1,52 @@
-const { exec } = require('child_process');
+import cfonts from 'cfonts';
+import chalk from 'chalk';
+const { say } = cfonts;
+import os from 'os';
+import { spawn } from 'child_process';
+const warn = `⚠️Warning ⚠️ Prediction Diagnosis Might be Wrong, You need to See a Vissible Doctor`;
+const log = `The bot was started...Lol goodluck silly boy!`;
+function startPythonScript() {
+    console.log('Halima-Bot Starting...');
+    const pythonProcess = spawn('python', ['Bot/main.py']);
 
-const command = 'python Bot/main.py';
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error executing command: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    // Check for specific error messages
-    if (stderr.includes("No internet connection")) {
-      console.error("Error: No internet connection. Please check your network settings.");
-    } else if (stderr.includes("Process stopped")) {
-      console.error("Process stopped by user.");
-    } else {
-      console.error(`Error output: ${stderr}`);
-    }
-    return;
-  }
-  console.log(`Output: ${stdout}`);
-});
+    say('Halima-Bot', {
+        font: 'chrome',
+        align: 'center',
+        gradient: ['red', 'magenta']
+      })
+      say(`Team Poison`, {
+        font: 'console',
+        align: 'center',
+        gradient: ['cyan', 'magenta']
+      })
+      
+
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(`Python stdout: ${data}`);
+    });
+
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`Python stderr: ${data}`);
+    });
+
+    pythonProcess.on('exit', (code) => {
+        console.log(`Python script exited with code ${code}. Restarting...`);
+        startPythonScript();
+    });
+
+}
+
+
+startPythonScript();
+console.log(chalk.yellow(`🖥️ ${os.type()}, ${os.release()} - ${os.arch()}`));
+const ramInGB = os.totalmem() / (1024 * 1024 * 1024);
+console.log(chalk.yellow(`💾 Total RAM: ${ramInGB.toFixed(2)} GB`));
+const freeRamInGB = os.freemem() / (1024 * 1024 * 1024);
+console.log(chalk.yellow(`💽 Free RAM: ${freeRamInGB.toFixed(2)} GB`));
+console.log(chalk.yellow(`📃 Script by Abdul`));
+
+console.log(warn);
+
+
+
